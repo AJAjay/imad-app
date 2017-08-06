0badaa75ac89dc63fd738243c12eb3ab94f7d156
AJAjay <ajajay271@gmail.com>
2017-08-06 22:59:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/aj-one.html',function(req,res){
+app.get('/ui/aj-one.html',function(req,res){
    res.sendFile(path.join(__dirname,'ui','aj-one.html'));
 });
 
