98d7fe881ebdab728deb0f9ce1adcd4f620bae38
AJAjay <ajajay271@gmail.com>
2017-08-28 19:53:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+//SOME java script
+
+var element = document.getElementById('main-text');
+element.innerHTML = 'New Value';
+
+//some code for image
+var img = document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
