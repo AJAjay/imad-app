12965fbde4d1b32fc6c4e8683b6ee7d64ba5a9c4
AJAjay <ajajay271@gmail.com>
2017-08-30 16:15:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var button = document.getElementById('counter');
 var counter =0;
 button.onclick = function(){
     counter = counter+1;
-    var span = document.getElemntById('count');
+    var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
 /*console.log('Loaded!');
