4c1a35825f8bf7c9172cdcee04da2e07f668fa2a
AJAjay <ajajay271@gmail.com>
2017-09-24 19:37:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'ajajay271',
+    database: 'ajajay271',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -45,6 +54,20 @@ function createtemplate(data){
 }  
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test.db',function(req,res){
+    //make a select template
+    //return a response with result
+   pool.query('SELECT * FORM test',function (err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else{
+           res.send(JSON.stringify(result));
+       }
+   }); 
 });
 var counter = 0;
 app.get('/counter',function(req,res){
