b665ea4031e91fa7d4a1b9d65bcada45145a004c
AJAjay <ajajay271@gmail.com>
2017-09-24 20:32:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/submit-name',function(req,res){//?name=xyz
     res.send(JSON.stringify(names));
 });
 app.get('/articles/:articleName',function(req,res){
-    pool.query("SELECT * FROM article WHERE title = '" +req.params.aritcleName + "'", function(err,result){
+    pool.query("SELECT * FROM article WHERE title = '" +req.params.articleName + "'", function(err,result){
         if(err){
             res.status(500).send(err.toString());
         } else{
