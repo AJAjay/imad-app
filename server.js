872be1683ccf690f51421bfbf08c3e6f72b7f062
AJAjay <ajajay271@gmail.com>
2017-09-24 19:38:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ var pool = new Pool(config);
 app.get('/test.db',function(req,res){
     //make a select template
     //return a response with result
-   pool.query('SELECT * FORM test',function (err, result){
+   pool.query('SELECT * FROM test',function (err, result){
        if(err){
            res.status(500).send(err.toString());
        }
